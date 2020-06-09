f01ebaa58ae90ddbc203115b703dbfd5ab7cb836
Krishnendu Mukherjee <kmukherjeejr@gmail.com>
2020-06-10 00:23:10 +0530

    Made huge changes to the website, added new pages

---
+++ submit-notice/recieve.php
@@ -0,0 +1,10 @@
+<?php
+$updatedData = $_POST['myJSON'];
+$data=file_get_contents('/json/jsonN.json');
+var_dump($data);
+$oldData=array_merge(array($updatedData),array(",\n"),array($data));
+// please validate the data you are expecting for security
+file_put_contents('jsonN.json', $oldData);
+//return the url to the saved file
+
+?>
--- submit-notice/submitNotice.js
+++ submit-notice/submitNotice.js
@@ -2,11 +2,23 @@ function submitAction(){
     var obj = {"heading" : document.getElementById("myTextarea1").value, "body" : document.getElementById("myTextarea2").value};
     var myJSON = JSON.stringify(obj);
     console.log(myJSON);
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    var theUrl = "jsonNotice.json";
-    xmlhttp.open("POST", theUrl);
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xmlhttp.send(myJSON);
-    //xmlhttp = new XMLHttpRequest();
+    jQuery.post('recieve.php', {
+    myJSON: myJSON
+}, function(response){
+    // response could contain the url of the newly saved file
+})
 }
+//     // var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+//     // var theUrl = "recieve.php";
+//     // xmlhttp.open("POST", theUrl);
+//     // xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//     // xmlhttp.send(myJSON);
+//     //xmlhttp = new XMLHttpRequest();
+// }
 
+// // var newData = JSON.stringify(data);
+// // jQuery.post('http://example.com/saveJson.php', {
+// //     newData: newData
+// // }, function(response){
+// //     // response could contain the url of the newly saved file
+// // })
