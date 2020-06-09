0849c46466b9d91496e5d79a88ac497e3e16a73a
Krishnendu Mukherjee <kmukherjeejr@gmail.com>
2020-06-10 01:19:23 +0530

    Updated Notice Board

--- notice/notice.js
+++ notice/notice.js
@@ -1,3 +1,34 @@
+const noticeList = document.getElementById("noticeList");
+let hpNotices = [];
+
+const loadNotices = async () => {
+  try {
+    const res = await fetch('/json/jsonN.json');
+    hpNotices = await res.json();
+    displayNotices(hpNotices);
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+const displayNotices = (notices) => {
+  const htmlString = notices
+      .map((notice) => {
+          return `
+          <button class="accordion">${notice.heading}</button>
+          <div class="panel">
+	            <p>${notice.body}</p>
+	        </div>
+          </li>
+      `;
+      })
+      .join('');
+  noticeList.innerHTML = htmlString;
+};
+
+loadNotices();
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
